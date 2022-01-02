007cd8d759f90449eaf525611fe19b04b0ee5930
jeong seonkyo <seonkyo0466@gmail.com>
2022-01-02 21:16:28 +0900

    mongodb 연결

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const productRoutes = require('./routes');
+const mongoose = require('mongoose');
 
-const PORT = 8080;
 const HOST = '0.0.0.0';
+const PORT = 8080;
+
+mongoose.connect('mongodb+srv://tjsry0466:!j14682533@cluster0.tk8cp.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        // useFindAndModify: false
+    })
+    .then(() => {
+        console.log('MongoDb Connected...');
+    })
+    .catch(err => console.log(err));
 
 const app = express();
 app.use(express.json());
