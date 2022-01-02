ea634a6c8169b0836eacac43785f2784d5b6dfec
jeong seonkyo <seonkyo0466@gmail.com>
2022-01-03 01:29:16 +0900

    before each 적용

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -5,16 +5,22 @@ const newProduct = require('../data/new-product.json');
 
 productModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
+
 describe('Product Controller Create', function () {
+    beforeEach(() => {
+        req.body = newProduct;
+    })
     it("should have a createProduct function", () => {
         expect(typeof productController.createProduct).toBe("function");
     })
 
     it("should call ProductModel.create", () => {
-        let req = httpMocks.createRequest();
-        let res = httpMocks.createResponse();
-        let next = null;
-        req.body = newProduct;
         productController.createProduct(req, res, next);
         expect(productModel.create).toBeCalledWith(newProduct);
     });
